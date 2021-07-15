a6c1732a7f63894261cb7cb4f87dac43ef819a2e
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2021-07-14 19:00:10 -0700

    arranging data on screen

--- web-module-project-async-redux/src/App.js
+++ web-module-project-async-redux/src/App.js
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { getWordResults } from "./actions";
 
-function App() {
+function App({
+  dispatch, 
+  searches = [],
+  error,
+  isFetching
+}) {
+
+  const [searchTerm, setSearchTerm] = useState('')
+  const [type, setType] = useState('')
+
+  if (error) {
+    return <h2>An error: {error}</h2>
+  }
+
+  if (isFetching) {
+    return (
+      <h2>Fetching Results...</h2>
+    )
+  }
+
   return (
     <div className="App">
-      <h1>Dictionary</h1>
+      <div className="Header">
+        <h1>Dictionary</h1> 
+      </div>
+      <form>
+        <span>Type Word Here:</span>
+        <input />
+        
+      </form>
     </div>
   );
 }
